8ff43841c796b567eaf76d49ee2fa4fa5092ab13
Pravin Timalsina <prabin.timilsina14@gmail.com>
2026-07-05 15:41:12 +0545

    format

--- src/main/java/org/openelisglobal/nidancisretry/NidanCisRetryClient.java
+++ src/main/java/org/openelisglobal/nidancisretry/NidanCisRetryClient.java
@@ -131,7 +131,8 @@ public class NidanCisRetryClient {
             return false;
         }
         String source = sourceObj.toString().trim();
-        if (!"CIS-PATIENTS-OPENELIS".equals(source) && !"CIS-PATIENTS-CDC-OPENELIS".equals(source) && !"CIS-ELIS-ORDERS".equals(source)) {
+        if (!"CIS-PATIENTS-OPENELIS".equals(source) && !"CIS-PATIENTS-CDC-OPENELIS".equals(source)
+                && !"CIS-ELIS-ORDERS".equals(source)) {
             return false;
         }
         Object statusObj = row.get("status");
